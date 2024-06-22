7e7a031b2000e09aa22a0202d96b9d5d0efe2f06
Rodrigo Mendez <rodrigomendezx777@gmail.com>
2024-06-22 03:49:49 -0300

    exercicios completos

--- ex01.js
+++ ex01.js
@@ -33,7 +33,7 @@ resultado3.innerText = textoMaiusculo
 // Transformar em Minúsculas
 // Escreva uma função que transforme uma string em letras minúsculas.
 
-function paraMinusculo (pagode) {
+function paraMinusculo(pagode) {
     return pagode.toLocaleLowerCase()
 }
 
@@ -44,10 +44,9 @@ resultado4.innerText = textoMinusculo
 // Extrair uma Substring
 // Escreva uma função que receba uma string e dois índices, e retorne a substring correspondente.
 
-function extractsubstring(extract,indexStart,indexEnd){
-    const result = extract.substring(indexStart,indexEnd)
+function extractsubstring(extract, indexStart, indexEnd) {
+    const result = extract.substring(indexStart, indexEnd)
     return result
-    
 }
 const frase = "Todos sabem que o Cristiano Ronaldo é melhor que o Messi"
 const extract = extractsubstring(frase, 18, 56)
@@ -57,16 +56,16 @@ resultado5.innerText = extract
 // Buscar Caracteres
 // Escreva uma função que receba uma string e um caractere, e retorne o índice da primeira ocorrência desse caractere na string. Se o caractere não for encontrado, retorne -1.
 
-function localizarString(){
+function localizarString() {
     let string = document.querySelector('#string').value
     let substring = document.querySelector('#substring').value
 
     let posicao = string.indexOf(substring)
     let resultado
 
-    if(posicao === -1){
+    if (posicao === -1) {
         resultado = 'achei não cria'
-    } else{
+    } else {
         resultado = 'A posição do elemento é: ' + posicao
     }
     document.getElementById('resultado6').innerHTML = resultado
@@ -75,29 +74,82 @@ function localizarString(){
 // Repetir String
 // Escreva uma função que receba uma string e um número, e retorne a string repetida esse número de vezes.
 
-const text = document.getElementById('text')
-const numero = document.getElementById('recebeNumber')
+function repeatString() {
+    const textForRepeat = document.getElementById('textForRepeat')
+    const numero = document.getElementById('recebeNumber')
 
-function repeatString(){
-    const result = text.value.repeat(numero.value);
-    document.getElementById('resultado7').innerText = result; 
+    const result = textForRepeat.value.repeat(numero.value);
+    document.getElementById('resultado7').innerText = result;
 }
 
 
 // Remover Espaços no Início e no Fim
 // Escreva uma função que remova os espaços em branco do início e do fim de uma string.
 
-function removeSpaces(){
-    const textoComEspaco = document.getElementById('spaceTxt').value
-    const resultadoSemEspaco = textoComEspaco.trim()
-    document.getElementById('resultado8').innerHTML = resultadoSemEspaco
+function actionRemoveSpaces(){
 
+    function removeSpaces(textoComEspaco) {
+    const resultadoSemEspaco = textoComEspaco.trim()
+    return resultadoSemEspaco
+}
+
+    const textoComEspaco = document.getElementById('spaceTxt').value
+    const resultRemoveSpaces = removeSpaces(textoComEspaco)
+    document.getElementById('resultado8').innerHTML = resultRemoveSpaces
 }
 
 // Substituir Caracteres
 // Escreva uma função que substitua todas as ocorrências de um caractere específico por outro em uma string.
 
 
+function gatilhoCaracteres() {
+    function trocarCaracteres(texto, caractereAntigo, caractereNovo) {
+
+        if (typeof texto !== 'string' || texto.length === 0) {
+            alert('texto inválido!');
+            return;
+        }
+
+        // Validação de caracteres
+        if (typeof caractereAntigo !== 'string' || caractereAntigo.length === 0) {
+            alert('Caractere antigo inválido!');
+            return;
+        }
+        if (typeof caractereNovo !== 'string' || caractereNovo.length === 0) {
+            alert('Caractere novo inválido!');
+            return;
+        }
+
+        // Conversão para expressão regular
+        const regex = new RegExp(caractereAntigo, "g");
+
+        // Substituição utilizando a expressão regular
+        const resultado = texto.replace(regex, caractereNovo);
+        return resultado;
+    }
+
+    const textoRecebido = document.getElementById('textoRecebido1').value
+    const caracter1 = document.getElementById('recebeCaractereParaSerTrocado').value
+    const caracter2 = document.getElementById('recebeCaractereParaTrocar').value
+    const resultadocarac = trocarCaracteres(textoRecebido, caracter1, caracter2)
+    document.getElementById('resultado8').innerHTML = resultadocarac
+}
+
 
 // Dividir String em Array
 // Escreva uma função que divida uma string em um array de substrings com base em um separador especificado.
+
+function actionDividirStrings() {
+    function dividirString(txtOriginal,separador) {
+    const dividirstring = txtOriginal.split(separador)
+    return dividirstring
+}
+
+    const DivitirStringTxt = document.getElementById('DivitirStringTxt').value
+    const separador = document.getElementById('separador').value
+    const stringDivididas = dividirString(DivitirStringTxt,separador)
+    
+    document.getElementById('resultado10').innerText = stringDivididas
+}
+
+
